4ac53039b3c7773332feb598f773e35923ff4aac
breemark <britomaximiliano83@gmail.com>
2023-04-22 19:51:48 +0700

    db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database',
+      entities: ['dist/**/*.entity{.ts,.js}'], // This generates the DB tables
+      synchronize: true,    // This synchronizes the DB
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
