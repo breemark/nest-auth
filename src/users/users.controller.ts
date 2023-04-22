83655be41c6be45ba95c26ff26f201c3ff434119
breemark <britomaximiliano83@gmail.com>
2023-04-22 20:17:25 +0700

    7. Register mock example

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,14 +3,21 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('api')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post('register')
+  register(@Body() body: any) {
+    return body;
+  }
+
+  /*
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+  */
 
   @Get()
   findAll() {
